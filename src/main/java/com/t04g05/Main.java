cca1ebf340443b0e9d4b21f348d8ba5e3d3db419
Tiago Oliveira <up202007448@up.pt>
2024-11-14 15:47:22 +0000

    Created src directory, updated gradle

---
+++ src/main/java/com/t04g05/Main.java
@@ -0,0 +1,7 @@
+package com.t04g05;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/t04g05/Dummy.java
@@ -0,0 +1,12 @@
+package com.t04g05;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Dummy {
+    @Test
+    public void test(){
+        assertTrue(true==false);
+    }
+}

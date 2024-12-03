0f4120a121a790489d44c01c4b7c5a51d530365e
Beatriz <beatriz@Ubuntu.myguest.virtualbox.org>
2024-12-03 22:35:42 +0000

    Adicionado a pasta gui

---
+++ src/main/java/gui/gui.java
@@ -0,0 +1,12 @@
+package gui;
+
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public class gui {
+}
+public void draw(TextGraphics graphics) {
+    graphics.setBackgroundColor(TextColor.Factory.fromString("#6E522C"));
+    graphics.setForegroundColor(TextColor.Factory.fromString("#FFFF33"));
+    graphics.putString(position.getX(), position.getY(), "X");
+}
